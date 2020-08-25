73ef823bb21b7bb148610e513dac8fa20ef601e6
Shaleen Arora <shaleenarora4@gmail.com>
2020-08-25 19:13:03 +0530

    bug fix

--- api/subcategories.js
+++ api/subcategories.js
@@ -5,66 +5,77 @@ const categorySchema = require("../schema/categorySchema");
 
 router.post('/create', function (req, res) {
     try {
-        const [parentCategory,subcategory]=[req.body.category,req.body.subcategory];
-        if(!parentCategory){
-            return res.json({msg:"Category name missing"})
+        const [parentCategory, subcategory] = [req.body.category, req.body.subcategory];
+        if (!parentCategory) {
+            return res.json({ msg: "Category name missing" })
         }
-        else{
-            categorySchema.findOne({name:parentCategory},function(error,result){
-                if(error){
-                    res.json({msg:'no such category exists'});
+        else {
+            categorySchema.findOne({ name: parentCategory }, function (error, result) {
+                if (error) {
+                    res.json({ msg: 'no such category exists' });
                 }
-                else{
-                    subcategorySchema.create({parentid:result._id,name:subcategory}).then(()=>
-                    res.json({msg:"addition success"})).catch((error)=>
-                    res.status(400).json({error:error}))
+                else {
+                    if (result) {
+                        subcategorySchema.create({ parentid: result._id, name: subcategory }).then(() =>
+                            res.json({ msg: "addition success" })).catch((error) =>
+                                res.status(400).json({ error: error }))
+                    }
+                    else {
+                        subcategorySchema.findOne({ name: parentCategory }, function (error, result) {
+                            if (result) {
+                                subcategorySchema.create({ parentid: result._id, name: subcategory }).then(() =>
+                                    res.json({ msg: "addition success" })).catch((error) =>
+                                        res.status(400).json({ error: error }))
+                            }
+                        })
+                    }
                 }
             })
         }
     } catch (error) {
-        return res.status(500).json({error:error});
+        return res.status(500).json({ error: error });
     }
 })
 
-router.get('/list',function(req,res){
+router.get('/list', function (req, res) {
     try {
-        subcategorySchema.find({}).then((data)=>
-        {res.json({subcategories:data})}).catch((error)=>
-        {res.status(500).json({error:error})});
+        subcategorySchema.find({}).then((data) => 
+        { res.json({ subcategories: data }) }).catch((error) => 
+        { res.status(500).json({ error: error }) });
     } catch (error) {
-        return res.status(500).json({error:error});
+        return res.status(500).json({ error: error });
     }
 })
 
-router.put('/update',function(req,res){
+router.put('/update', function (req, res) {
     try {
-        const {name,updatedName}=req.body;
-        if(!name){
-            return res.json({msg:'No category entered'});
+        const { name, updatedName } = req.body;
+        if (!name) {
+            return res.json({ msg: 'No category entered' });
         }
-        else{
-            subcategorySchema.findOneAndUpdate({name:name},{name:updatedName}).then(()=> 
-            res.json({msg:"updation success"})).catch(()=>
-            res.json({msg:"No such subcategory exists"}));
+        else {
+            subcategorySchema.findOneAndUpdate({ name: name }, { name: updatedName }).then(() =>
+                res.json({ msg: "updation success" })).catch(() =>
+                    res.json({ msg: "No such subcategory exists" }));
         }
     } catch (error) {
-        return res.status(500).json({error:error});
+        return res.status(500).json({ error: error });
     }
 })
 
-router.delete('/delete',function(req,res){
+router.delete('/delete', function (req, res) {
     try {
-        const {name}=req.body;
-        if(!name){
-            return res.json({msg:'No category entered'});
+        const { name } = req.body;
+        if (!name) {
+            return res.json({ msg: 'No category entered' });
         }
-        else{
-            subcategorySchema.findOneAndRemove({name:name}).then(()=> 
-            res.json({msg:"deletion success"})).catch(()=>
-            res.json({msg:"No such subcategory exists"}));
+        else {
+            subcategorySchema.findOneAndRemove({ name: name }).then(() =>
+                res.json({ msg: "deletion success" })).catch(() =>
+                    res.json({ msg: "No such subcategory exists" }));
         }
     } catch (error) {
-        return res.status(500).json({error:error});
+        return res.status(500).json({ error: error });
     }
 })
 
